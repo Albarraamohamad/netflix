07518b0a5210b3a5b8dbf9b18d89e4902118d70f
Albarraa Mohamed <160414633+Albarraamohamad@users.noreply.github.com>
2025-03-08 01:42:26 +0200

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -97,6 +97,9 @@ const Navbar = () => {
 
                   </Link>
                 </li>
+                <li className="px-4 py-2 hover:bg-gray-700 cursor-pointer">
+                  <Link to='/Children'>Children</Link>
+                </li>
               </ul>
             </div>
           )}
